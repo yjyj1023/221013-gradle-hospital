9b2588e49df1ec8703c8ca6bd3ade8d323bfb992
yjyj1023 <lyj19961023@gmail.com>
2022-10-13 13:21:24 +0900

    address를 추가해서 test

--- src/main/java/org/example/HospitalParser.java
+++ src/main/java/org/example/HospitalParser.java
@@ -2,13 +2,11 @@ package org.example;
 
 
 public class HospitalParser implements Parser<Hospital>{
-    private String replaceAllQuot(String str){
-        return str.replaceAll("\"", "");
-    }
     @Override
     public Hospital parse(String str) {
+        str = str.replaceAll("\"", "");
         String[] splitted = str.split(",");
 
-        return new Hospital(replaceAllQuot(splitted[0]), replaceAllQuot(splitted[1]));
+        return new Hospital(splitted[0], splitted[1]);
     }
 }
